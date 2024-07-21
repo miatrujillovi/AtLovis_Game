f51b4b58c37f1d6b7ee741cac4776c164e27c653
Mia Trujillo <mia.trujillo@uniat.edu.mx>
2024-07-20 19:27:38 -0600

    Cambios de Movimiento y Colisiones
    - Se arreglo el script de PlayerMovement
    - Se agregaron colisiones a los objetos correspondientes
    - Se agregaron los Sprites de los Personajes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float characterSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Vector3 targetPosition;
+    public LayerMask solidObjects;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if(IsWalkable(targetPosition))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 15);
+        }
+    }
+
+    private bool IsWalkable(Vector3 _targetPosition)
+    {
+        if(Physics2D.OverlapCircle(_targetPosition, 0.2f, solidObjects) != null)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
