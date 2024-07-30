b122e2df9263ca2b095603051779fed920768d35
Mia Trujillo <mia.trujillo@uniat.edu.mx>
2024-07-30 06:50:27 -0600

    Correcciones Finales en Gameplay
    - Se arreglaron funciones de TimerManager.cs

--- Assets/Scripts/Manager/TimerManager.cs
+++ Assets/Scripts/Manager/TimerManager.cs
@@ -56,8 +56,8 @@ public class TimerManager : MonoBehaviour
     {
         Debug.Log("Corrutina Countdown iniciada");
 
-        Debug.Log("Esperando 10 segundos...");
-        yield return new WaitForSeconds(10);
+        Debug.Log("Esperando 120 segundos...");
+        yield return new WaitForSeconds(120);
 
         Debug.Log("10 segundos han pasado");
         // Variable de Misión General
@@ -77,22 +77,27 @@ public class TimerManager : MonoBehaviour
     {
         Debug.Log("Calculando propina...");
 
-        if (GlobalVariableManager.clientesAtendidos <= 3)
+
+        if (GlobalVariableManager.clientesAtendidos == 0)
+        {
+            propinaTxt.text = "No Ganaste Propinas";
+        }
+        else if (GlobalVariableManager.clientesAtendidos >= 1 && GlobalVariableManager.clientesAtendidos <= 3)
         {
             GlobalVariableManager.propinaInventory += GlobalVariableManager.earnedPropina[0];
             propinaTxt.text = $"Propina Ganada: {GlobalVariableManager.earnedPropina[0]}";
-        }
-        else if (GlobalVariableManager.clientesAtendidos <= 6)
+        } 
+        else if (GlobalVariableManager.clientesAtendidos >= 4 && GlobalVariableManager.clientesAtendidos <= 6)
         {
             GlobalVariableManager.propinaInventory += GlobalVariableManager.earnedPropina[1];
             propinaTxt.text = $"Propina Ganada: {GlobalVariableManager.earnedPropina[1]}";
-        }
-        else if (GlobalVariableManager.clientesAtendidos <= 10)
+        } 
+        else if (GlobalVariableManager.clientesAtendidos >= 7 && GlobalVariableManager.clientesAtendidos <= 10)
         {
             GlobalVariableManager.propinaInventory += GlobalVariableManager.earnedPropina[2];
             propinaTxt.text = $"Propina Ganada: {GlobalVariableManager.earnedPropina[2]}";
-        }
-        else if (GlobalVariableManager.clientesAtendidos <= 16)
+        } 
+        else if (GlobalVariableManager.clientesAtendidos >= 11)
         {
             if (!MisionsManager.OnceClientes)
             {
@@ -107,22 +112,26 @@ public class TimerManager : MonoBehaviour
     {
         Debug.Log("Calculando experiencia...");
 
-        if (GlobalVariableManager.clientesAtendidos <= 3)
+        if (GlobalVariableManager.clientesAtendidos == 0)
+        {
+            experienciaTxt.text = "No Ganaste Experiencia";
+        } 
+        else if (GlobalVariableManager.clientesAtendidos >= 1 && GlobalVariableManager.clientesAtendidos <= 3)
         {
             GlobalVariableManager.experienciaInventory += GlobalVariableManager.earnedExperiencia[0] + GlobalVariableManager.extraExpDecoraciones[0];
             experienciaTxt.text = $"Experiencia Ganada: {GlobalVariableManager.earnedExperiencia[0]}";
-        }
-        else if (GlobalVariableManager.clientesAtendidos <= 6)
+        } 
+        else if (GlobalVariableManager.clientesAtendidos >= 4 && GlobalVariableManager.clientesAtendidos <= 6)
         {
             GlobalVariableManager.experienciaInventory += GlobalVariableManager.earnedExperiencia[1] + GlobalVariableManager.extraExpDecoraciones[1];
             experienciaTxt.text = $"Experiencia Ganada: {GlobalVariableManager.earnedExperiencia[1]}";
-        }
-        else if (GlobalVariableManager.clientesAtendidos <= 10)
+        } 
+        else if (GlobalVariableManager.clientesAtendidos >= 7 && GlobalVariableManager.clientesAtendidos <= 10)
         {
             GlobalVariableManager.experienciaInventory += GlobalVariableManager.earnedExperiencia[2] + GlobalVariableManager.extraExpDecoraciones[2];
             experienciaTxt.text = $"Experiencia Ganada: {GlobalVariableManager.earnedExperiencia[2]}";
-        }
-        else if (GlobalVariableManager.clientesAtendidos <= 16)
+        } 
+        else if (GlobalVariableManager.clientesAtendidos >= 11)
         {
             GlobalVariableManager.experienciaInventory += GlobalVariableManager.earnedExperiencia[3] + GlobalVariableManager.extraExpDecoraciones[3];
             experienciaTxt.text = $"Experiencia Ganada: {GlobalVariableManager.earnedExperiencia[3]}";
